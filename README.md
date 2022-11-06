150443ed9c763e2cf9b54de9a875b5f0e499b760
Alexander Gaal <alexander.gaal@live.at>
2022-11-06 14:50:27 +0100

    🐚

---
+++ README.md
@@ -0,0 +1,12 @@
+# Magische Miesmuschel
+
+Die Magische Miesmuschel ist ein Discord-Bot, der Fragen die mit "MM:" beginenn mit Yes, No und Maybe beantwortet. Um die Antworten und GIFs zu generieren wird die [yesno.wtf](https://yesno.wtf)-API genutzt.
+
+## Installation
+1. Installiere die Abhängigkeiten mit `npm install`.
+2. Erstelle eine `.env`. Du kannst hierfür die `.env.example` kopieren.
+3. Hinterlege den Token deines Bots in der `.env` unter `DISCORD_TOKEN`.
+4. Hoste den Bot und starte ihn mit `node index.mjs`.
+
+## Contributors
+- [alexgaal](https://github.com/alexgaal)
---
+++ index.mjs
@@ -0,0 +1,44 @@
+import * as dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import {REST, SlashCommandBuilder, Routes} from "discord.js";
+import discord from "discord.js";
+
+dotenv.config();
+
+const token = process.env.DISCORD_TOKEN;
+
+const {Client, GatewayIntentBits} = discord;
+
+const intents = [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+];
+
+const client = new Client({intents});
+
+const yesnoApiUrl = "https://yesno.wtf/api";
+
+client.login(token);
+client.once("ready", () => console.log(`Logged in as ${client.user.tag}`));
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (!message.content.includes("MM:")) {
+        return;
+    }
+
+    const question = message.content.split("MM:")[1].trim();
+
+    console.log(`${message.author.username} asked "${question}"`);
+
+    fetch(yesnoApiUrl)
+        .then((response) => response.json())
+        .then((response) => message.reply({
+            content: response.answer,
+            files: [response.image]
+        }));
+});
